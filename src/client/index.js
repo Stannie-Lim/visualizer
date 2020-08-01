f0761eb523c9d071feca41ccca8dfece5d4edca7
Stanley <lims95@yahoo.com>
2020-07-31 21:00:55 -0400

    sockets

--- src/client/index.js
+++ src/client/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import App from './App';
-import store from './store/store';
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import store from "./store/store";
 
-render(<Provider store={ store }><App /></Provider>, document.querySelector('#root'));
+import "./socket";
 
-
-
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
--- src/server/index.js
+++ src/server/index.js
@@ -1,32 +1,35 @@
-const express = require('express');
+const path = require("path");
+const db = require("./db/db");
+const morgan = require("morgan");
+const express = require("express");
+const socketio = require("socket.io");
 const app = express();
-const path = require('path');
-const db = require('./db/db');
 
+app.use(morgan("dev"));
 app.use(express.json());
 
-const staticFolder = path.join(__dirname, '..', '..', 'static')
-const distFolder = path.join(__dirname, '..', '..', 'dist')
-app.use(express.static(staticFolder))
-app.use(express.static(distFolder))
+const staticFolder = path.join(__dirname, "..", "..", "static");
+const distFolder = path.join(__dirname, "..", "..", "dist");
+app.use(express.static(staticFolder));
+app.use(express.static(distFolder));
 
-app.get('/', (req, res, next) => {
-  res.sendFile(path.join(staticFolder, 'index.html'))
+app.get("/", (req, res, next) => {
+  res.sendFile(path.join(staticFolder, "index.html"));
 });
 
 // api routes
-app.use('/api', require('./api'));
+app.use("/api", require("./api"));
 
 // error middleware
-app.use((err, req, res, next)=> {
+app.use((err, req, res, next) => {
   console.error(err);
-  res.status(500).send({ message: err. message });
+  res.status(500).send({ message: err.message });
 });
-
 
 const port = process.env.PORT || 3000;
 
-db.sync()
-  .then(()=> {
-    app.listen(port);
-  });
+db.sync().then(() => {
+  const server = app.listen(port);
+  const io = socketio(server);
+  require("./socket")(io);
+});
